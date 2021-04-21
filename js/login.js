443870145fc9338519018a3f5856d5a48b9862a2
Susanta-Adak <susantaadak513@gmail.com>
2021-04-21 06:43:40 +0530

    first commit

---
+++ js/login.js
@@ -0,0 +1,34 @@
+$(document).ready(()=>{
+    
+    $("#admin_login_btn").click(()=>{
+        $("#login_div_section").show();
+        $("body").css("overflow","hidden");
+    });
+    $("#cloce_icon").click(()=>{
+        $("#login_div_section").hide();
+        $("body").css("overflow","scroll");
+    });
+    /*CHECK USER AND PASSWORD*/
+    $("#loginUser").click(()=>{
+        if($("#login_username").val()=="susanta"){
+            if($("#login_pass").val()=="123"){
+               
+                // window.location.replace("pages/Admin/adminhome.html"); 
+                window.open('pages/Admin/adminhome.html','_self');
+                return false;
+            }
+            else{
+                $("#alert_user2").show();
+                setInterval(()=>{
+                    $("#alert_user2").hide();
+                },3000);
+            }
+        }
+        else{
+            $("#alert_user1").show();
+            setInterval(()=>{
+                $("#alert_user1").hide();
+            },3000);
+        }
+    });
+});
