f980cdd513a53007930a377d468fd704fba2cbd7
Susanta-Adak <susantaadak513@gmail.com>
2021-05-04 12:18:32 +0530

    add bowlerScoreUpdate

--- js/index.js
+++ js/index.js
@@ -52,42 +52,47 @@ $(document).ready(() => {
         div1_b,
         div1_4s,
         div1_6s
-        );
-
-      var div1 = $("<div class='table_row'></div>").append(
-        div1_nm,
-        div1_stat
       );
-      var div2 = $("<div class='hr_line_for_scoreboard'></div>").text('');
-      
+
+      var div1 = $("<div class='table_row'></div>").append(div1_nm, div1_stat);
+      var div2 = $("<div class='hr_line_for_scoreboard'></div>").text("");
+
       $("#last_batting_score_tableid").append(div1, div2);
 
       total = total + json_extract.Run + json_extract.Extras;
       totalBall = totalBall + json_extract.Ball;
       totalExtra = totalExtra + json_extract.Extras;
-      if(json_extract.Status!=="not out"){
-        wicket=wicket+1;
+      if (json_extract.Status !== "not out") {
+        wicket = wicket + 1;
       }
     }
   }
   var extxt = $("<div class='text_e'></div>").text("Extras");
   var tottxt = $("<div class='txt_t'></div>").text("Total");
-  var exval=$("<div class='val_e'></div>").text(totalExtra);
-  var totval=$("<div class='val_t'></div>").text(total);
+  var exval = $("<div class='val_e'></div>").text(totalExtra);
+  var totval = $("<div class='val_t'></div>").text(total);
 
-  var div1f = $("<div class='extra_run'></div>").append(extxt,exval);
-  var div2f = $("<div class='hr_line_for_scoreboard'></div>").text('');
-  var div3f = $("<div class='total_run'></div>").append(tottxt,totval);
+  var div1f = $("<div class='extra_run'></div>").append(extxt, exval);
+  var div2f = $("<div class='hr_line_for_scoreboard'></div>").text("");
+  var div3f = $("<div class='total_run'></div>").append(tottxt, totval);
 
-  
-  var footerScore=$("<div class='footer_r'></div>").append(
+  var footerScore = $("<div class='footer_r'></div>").append(
     div1f,
     div2f,
     div3f
   );
   $("#last_batting_score_tableid").append(footerScore);
-  
-  var ft_val=$("<div class='first_team_name'></div>").text(total+"/"+wicket+" ("+Math.floor(totalBall/6)+"."+totalBall%6+")");
+
+  var ft_val = $("<div class='first_team_name'></div>").text(
+    total +
+      "/" +
+      wicket +
+      " (" +
+      Math.floor(totalBall / 6) +
+      "." +
+      (totalBall % 6) +
+      ")"
+  );
   $("#team1_title").append(ft_val);
 });
 
@@ -125,41 +130,151 @@ $(document).ready(() => {
         div1_b,
         div1_4s,
         div1_6s
-        );
-
-      var div1 = $("<div class='table_row'></div>").append(
-        div1_nm,
-        div1_stat
       );
-      var div2 = $("<div class='hr_line_for_scoreboard'></div>").text('');
-      
+
+      var div1 = $("<div class='table_row'></div>").append(div1_nm, div1_stat);
+      var div2 = $("<div class='hr_line_for_scoreboard'></div>").text("");
+
       $("#first_batting_score_tableid").append(div1, div2);
 
       total = total + json_extract.Run + json_extract.Extras;
       totalBall = totalBall + json_extract.Ball;
       totalExtra = totalExtra + json_extract.Extras;
-      if(json_extract.Status!=="not out"){
-        wicket=wicket+1;
+      if (json_extract.Status !== "not out") {
+        wicket = wicket + 1;
       }
     }
   }
   var extxt = $("<div class='text_e'></div>").text("Extras");
   var tottxt = $("<div class='txt_t'></div>").text("Total");
-  var exval=$("<div class='val_e'></div>").text(totalExtra);
-  var totval=$("<div class='val_t'></div>").text(total);
+  var exval = $("<div class='val_e'></div>").text(totalExtra);
+  var totval = $("<div class='val_t'></div>").text(total);
 
-  var div1f = $("<div class='extra_run'></div>").append(extxt,exval);
-  var div2f = $("<div class='hr_line_for_scoreboard'></div>").text('');
-  var div3f = $("<div class='total_run'></div>").append(tottxt,totval);
+  var div1f = $("<div class='extra_run'></div>").append(extxt, exval);
+  var div2f = $("<div class='hr_line_for_scoreboard'></div>").text("");
+  var div3f = $("<div class='total_run'></div>").append(tottxt, totval);
 
-  
-  var footerScore=$("<div class='footer_r'></div>").append(
+  var footerScore = $("<div class='footer_r'></div>").append(
     div1f,
     div2f,
     div3f
   );
   $("#first_batting_score_tableid").append(footerScore);
-  
-  var ft_val=$("<div class='first_team_name'></div>").text(total+"/"+wicket+" ("+Math.floor(totalBall/6)+"."+totalBall%6+")");
+
+  var ft_val = $("<div class='first_team_name'></div>").text(
+    total +
+      "/" +
+      wicket +
+      " (" +
+      Math.floor(totalBall / 6) +
+      "." +
+      (totalBall % 6) +
+      ")"
+  );
   $("#team2_title").append(ft_val);
+});
+
+/* PREAVIEW SCORE*/
+$(document).ready(() => {
+  var i;
+  
+  for (i = 0; i < localStorage.length; i++) {
+    var keys = localStorage.key(i);
+    if (keys.match("SeBowler")) {
+      var json_text = localStorage.getItem(keys);
+      var json_extract = JSON.parse(json_text);
+
+      var div2_o = $("<div class='table_column2'></div>").text(
+        Math.floor(json_extract.Ball/6)
+      );
+      var div2_m = $("<div class='table_column2'></div>").text(
+        0
+      );
+      var div2_r = $("<div class='table_column2'></div>").text(
+        json_extract.Run
+      );
+      var div2_w = $("<div class='table_column2'></div>").text(
+        json_extract.Wicket
+      );
+      var div2_nb = $("<div class='table_column2'></div>").text(
+          json_extract.NoBall
+        );
+      var div2_wd = $("<div class='table_column2'></div>").text(
+      json_extract.WideBall
+      );
+
+      
+      
+
+      var div1 = $("<div class='table_column1'></div>").text(json_extract.Name);
+      
+      var div2 = $("<div class='score_headingid'></div>").append(
+        div2_o,
+        div2_m,
+        div2_r,
+        div2_w,
+        div2_nb,
+        div2_wd
+      );
+      
+      var row = $("<div class='table_row'></div>").append(div1, div2);
+      var line=$("<div class='hr_line_for_scoreboard'></div>").text("");
+      $("#last_bowling_score_tableid").append(row,line);
+
+      
+    }
+  }
+  
+});
+/**PREVIEW SECOND BOWLING SCORE */
+$(document).ready(() => {
+  var i;
+  
+  for (i = 0; i < localStorage.length; i++) {
+    var keys = localStorage.key(i);
+    if (keys.match("firstBowler")) {
+      var json_text = localStorage.getItem(keys);
+      var json_extract = JSON.parse(json_text);
+
+      var div2_o = $("<div class='table_column2'></div>").text(
+        Math.floor(json_extract.Ball/6)
+      );
+      var div2_m = $("<div class='table_column2'></div>").text(
+        0
+      );
+      var div2_r = $("<div class='table_column2'></div>").text(
+        json_extract.Run
+      );
+      var div2_w = $("<div class='table_column2'></div>").text(
+        json_extract.Wicket
+      );
+      var div2_nb = $("<div class='table_column2'></div>").text(
+          json_extract.NoBall
+        );
+      var div2_wd = $("<div class='table_column2'></div>").text(
+      json_extract.WideBall
+      );
+
+      
+      
+
+      var div1 = $("<div class='table_column1'></div>").text(json_extract.Name);
+      
+      var div2 = $("<div class='score_headingid'></div>").append(
+        div2_o,
+        div2_m,
+        div2_r,
+        div2_w,
+        div2_nb,
+        div2_wd
+      );
+      
+      var row = $("<div class='table_row'></div>").append(div1, div2);
+      var line=$("<div class='hr_line_for_scoreboard'></div>").text("");
+      $("#first_bowling_score_tableid").append(row,line);
+
+      
+    }
+  }
+  
 });
--- pages/Admin/cricket/OperateBowling/bowlingJs/bowling.js
+++ pages/Admin/cricket/OperateBowling/bowlingJs/bowling.js
@@ -351,3 +351,160 @@ $(document).ready(() => {
     //   footerScore
     // );
   });
+
+
+  /*SECOND TEAM*/
+
+  /*THIS IS FOR FIRST INNINGS*/
+  $(document).ready(() => {
+    $("#add2").click(() => {
+      var name = $("#batting_name2").val();
+      var noball = parseInt($("#noball2").val());
+      var wideball = parseInt($("#wideball2").val());
+      var wicket = parseInt($("#wicket2").val());
+      var numberOf4s = parseInt($("#count_4s2").val());
+      var numberOf6s = parseInt($("#count_6s2").val());
+      var otherRun = parseInt($("#other_run2").val());
+     
+      
+
+      if (localStorage.getItem(name + "SeBowler") != null) {
+        var json_text = localStorage.getItem(name + "SeBowler");
+        var json_extract = JSON.parse(json_text);
+        var run = json_extract.Run+ numberOf4s * 4 + numberOf6s * 6 + otherRun+noball+wideball;
+        
+        var ball=json_extract.Ball;
+        if(noball==0 && wideball==0){
+            ball = ball + 1;
+        }
+        var wicket=wicket+json_extract.Wicket;
+        
+        var numberOf4s = json_extract.NumberOf4s + numberOf4s;
+        var numberOf6s = json_extract.NumberOf6s + numberOf6s;
+        var noball=json_extract.NoBall+noball;
+        var wideball=json_extract.WideBall+wideball;
+        
+        var addDetails = {
+            Name: name,
+            NoBall:noball,
+            WideBall:wideball,
+            NumberOf4s: numberOf4s,
+            NumberOf6s: numberOf6s,
+            OtherRun: otherRun,
+            Run: run,
+            Ball: ball,
+            Wicket:wicket
+        };
+        var addDetails_data = JSON.stringify(addDetails);
+        localStorage.setItem(name + "SeBowler", addDetails_data);
+      } else {
+        var run = numberOf4s * 4 + numberOf6s * 6 + otherRun+noball+wideball;
+        var ball=0;
+        if(noball==0 && wideball==0){
+            ball=1;
+        }
+        
+
+        var addDetails = {
+          Name: name,
+          NoBall:noball,
+          WideBall:wideball,
+          NumberOf4s: numberOf4s,
+          NumberOf6s: numberOf6s,
+          OtherRun: otherRun,
+          Run: run,
+          Ball: ball,
+          Wicket:wicket
+        };
+        var addDetails_data = JSON.stringify(addDetails);
+        localStorage.setItem(name + "SeBowler", addDetails_data);
+      }
+
+      window.location = location.href;
+    });
+  });
+  /* PREAVIEW SCORE*/
+  $(document).ready(() => {
+    var i;
+    // var total = 0;
+    // var totalBall = 0;
+    // var totalExtra = 0;
+    // var wicket = 0;
+    for (i = 0; i < localStorage.length; i++) {
+      var keys = localStorage.key(i);
+      if (keys.match("SeBowler")) {
+        var json_text = localStorage.getItem(keys);
+        var json_extract = JSON.parse(json_text);
+
+        var div2_o = $("<div class='table_column2'></div>").text(
+          Math.floor(json_extract.Ball/6)
+        );
+        var div2_m = $("<div class='table_column2'></div>").text(
+          0
+        );
+        var div2_r = $("<div class='table_column2'></div>").text(
+          json_extract.Run
+        );
+        var div2_w = $("<div class='table_column2'></div>").text(
+          json_extract.Wicket
+        );
+        var div2_nb = $("<div class='table_column2'></div>").text(
+            json_extract.NoBall
+          );
+        var div2_wd = $("<div class='table_column2'></div>").text(
+        json_extract.WideBall
+        );
+
+        var div1_nm = $("<p class='p_nm'></p>").text(json_extract.Name);
+        
+
+        var div1 = $("<div class='table_column1'></div>").append(
+          div1_nm
+          
+        );
+        var div2 = $("<div class='score_headingid2'></div>").append(
+          div2_o,
+          div2_m,
+          div2_r,
+          div2_w,
+          div2_nb,
+          div2_wd
+        );
+        // var div3 = $(
+        //   "<div style='width: 93%; height: 2px;border: 1px solid #fff;background-color: gray;'></div>"
+        // ).text();
+        var row = $("<div class='table_row2'></div>").append(div1, div2);
+        $(".second_team_score_preview3").append(row);
+
+        // total = total + json_extract.Run + json_extract.Extras;
+        // totalBall = totalBall + json_extract.Ball;
+        // totalExtra = totalExtra + json_extract.Extras;
+      }
+    }
+    // var name1 = $("<p class='name_txt'></p>").text("Extras");
+    // var name2 = $("<p class='name_txt'></p>").text("Total");
+
+    // var val1 = $("<p class='value_txt'></p>").text(totalExtra);
+    // var val2 = $("<p class='value_txt'></p>").text(total);
+    // var div4 = $("<div class='hr_line_for_scoreboard'></div>").text();
+
+    // var footerSection1 = $("<div class='footer_score'></div>").append(
+    //   name1,
+    //   val1
+    // );
+    // var footerSection2 = $("<div class='footer_score'></div>").append(
+    //   div4,
+    //   name2,
+    //   val2
+    // );
+
+    // var total_rn = $("<p class='run_footer_prev'></p>").text(
+    //   total + "/" + wicket + " (0." + totalBall + ")"
+    // );
+    // var footerScore = $("<div class='footer_score'></div>").append(total_rn);
+    // var scorePreviewSection = $(".first_team_score_preview2").append(
+    //   footerSection1,
+    //   footerSection2,
+    //   footerScore
+    // );
+  });
