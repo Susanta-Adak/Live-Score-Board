895f5d75ac80f432630c198c6b74a0280562c5be
Susanta-Adak <susantaadak513@gmail.com>
2021-05-05 20:17:45 +0530

    add schedule

--- pages/Schedule/scheduleJS/schedule.js
+++ pages/Schedule/scheduleJS/schedule.js
@@ -17,3 +17,154 @@ $("#team_name_label2").click(function () {
   $("#score_table_for_lastteam").show();
   $("#score_table_for_firstteam").hide();
 });
+
+/**HEADER BUTTON SECTION */
+$(document).ready(()=>{
+  $("#domestic_schedule").hide();
+  $("#t20_schedule").hide();
+  $("#women_schedule").hide();
+  $("#all_schedule").hide();
+  $("#btn1").css({
+    "background-color":"rgb(0, 94, 0)",
+    "color":"white"
+  });
+  $("#btn1").click(()=>{
+    $("#domestic_schedule").hide();
+    $("#inter_schedule").show();
+    $("#women_schedule").hide();
+    $("#all_schedule").hide();
+    $("#t20_schedule").hide();
+    $("#btn1").css({
+      "background-color":"rgb(0, 94, 0)",
+      "color":"white"
+    });
+    $("#btn2").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn3").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn5").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn4").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+  });
+
+  $("#btn2").click(()=>{
+    $("#domestic_schedule").show();
+    $("#inter_schedule").hide();
+    $("#women_schedule").hide();
+    $("#all_schedule").hide();
+    $("#t20_schedule").hide();
+    $("#btn2").css({
+      "background-color":"rgb(0, 94, 0)",
+      "color":"white"
+    });
+    $("#btn1").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn3").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn5").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn4").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+  });
+  
+  $("#btn3").click(()=>{
+    $("#domestic_schedule").hide();
+    $("#inter_schedule").hide();
+    $("#women_schedule").hide();
+    $("#all_schedule").hide();
+    $("#t20_schedule").show();
+    $("#btn1").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn2").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn3").css({
+      "background-color":"rgb(0, 94, 0)",
+      "color":"white"
+    });
+    $("#btn5").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn4").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+  });
+
+  $("#btn4").click(()=>{
+    $("#domestic_schedule").hide();
+    $("#inter_schedule").hide();
+    $("#t20_schedule").hide();
+    $("#all_schedule").hide();
+    $("#women_schedule").show();
+    $("#btn1").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn2").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn3").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn5").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn4").css({
+      "background-color":"rgb(0, 94, 0)",
+      "color":"white"
+    });
+  });
+
+  $("#btn5").click(()=>{
+    $("#domestic_schedule").hide();
+    $("#inter_schedule").hide();
+    $("#t20_schedule").hide();
+    $("#women_schedule").hide();
+    $("#all_schedule").show();
+    $("#btn1").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn2").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn3").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn4").css({
+      "background-color":"#fff",
+      "color":"black"
+    });
+    $("#btn5").css({
+      "background-color":"rgb(0, 94, 0)",
+      "color":"white"
+    });
+  });
+});
